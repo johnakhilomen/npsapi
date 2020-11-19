d7dd32d1b56f40228ccfbaccc6f8153fa5a374ee
John Akhilomen <jonbasanti@gmail.com>
2020-11-19 13:12:53 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-"use strict";
-
-const apiKey = "kSKFJRm1oQyKlvLhETSmThg6zQVDeYpGjcwJDoJd";
-const searchURL="https://api.nps.gov/api/v1/parks";
+'use strict';
+// put your own value below!
+const apiKey = "xK8CdTImrEHLFRftUhG6MlpPHiLeQEd7R9hnxCt1";
+const searchURL = "https://developer.nps.gov/api/v1/parks";
 
 function submitForm(){
-  $('#parkForm').submit(e => {
-    e.preventDefault();
-    if(!/^\w(\s*,?\s*\w)*$/.test($("#state-name-input").val())){
+  $('#parkForm').submit(event => {
+    event.preventDefault();
+    if(!/^\w(\s*,?\s*\w)*$/.test($("#stateInput").val())){
       alert("Invalid input");
       return;
     }
-    const userInput = $('#state-name-input').val().split(",");
-    const numResults = $('#max-results-input').val();
+    const userInput = $('#stateInput').val().split(",");
+    const numResults = $('#resultInput').val();
     getParkResults(userInput, numResults);
   });
 }
@@ -23,7 +23,7 @@ function formatQueryParams(params){
 //GET Request to NPS API
 function getParkResults(query, maxResults){
   const params = {
-    key: apiKey,
+    api_key: apiKey,
     stateCode: query,      
     limit: maxResults
   };
@@ -36,13 +36,16 @@ function getParkResults(query, maxResults){
     .catch(err =>alert(err));    
 }
 //Render the GET Request results to the DOM
-function renderParkResults(parkList){      
+function renderParkResults(parkList){    
+  console.log(parkList)  
   $('#results-list').empty();
   $('#results').text("Check your search result below:");
   parkList.forEach(item =>{
-  $('#results-list').append(`<li><h3>${item.fullName}</h3>
+  $('#results').append(`<li><h3>${item.fullName}</h3>
   <p>${item.description}</p><a href=${item.url}>Park's Website</a><div id="addresses"><h5>${item.addresses[0].type}:</h5><p class="addresses">${item.addresses[0].line1}</p><p class="addresses">${item.addresses[0].line2}</p><p class="addresses">${item.addresses[0].line3}</p>
-  <p class="addresses">${item.addresses[0].city}, ${item.addresses[0].stateCode}, ${item.addresses[0].postalCode}</p></div></li>`)
+  <p class="addresses">${item.addresses[0].city}, ${item.addresses[0].stateCode}, ${item.addresses[0].postalCode}</p></div>
+  <p class="states">${item.states}</p></div>
+  </li>`)
 });
 }
 
